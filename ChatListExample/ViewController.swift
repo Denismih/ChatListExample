746bc4a2cd7e38291922d243256f6e17ee078515
DenisMih <Denismih@gmail.com>
2019-10-02 11:30:45 +0300

    Initial Commit

---
+++ ChatListExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatListExample
+//
+//  Created by Denis Mikhaylovskiy  on 02/10/2019.
+//  Copyright © 2019 ru.2nets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
